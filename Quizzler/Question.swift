6b9028b10952b1a95d4ab562b780ea9d534323c1
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-11-14 23:07:59 +0200

    Implement Model

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Konstantin Konstantinov on 11/14/17.
+//  Copyright © 2017 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var correctAnswer: Bool
+}

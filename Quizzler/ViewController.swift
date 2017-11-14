611d6791fb6f2debabc98f2afabaa8e9ddcaad4d
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-11-14 22:50:48 +0200

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Konstantin Konstantinov on 11/8/17.
+//  Copyright © 2017 Konstantin Konstantinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
